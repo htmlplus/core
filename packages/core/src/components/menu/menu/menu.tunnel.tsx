a602a0dc2fd9e70899fbc717aa421ce55342f986
mohsen babaei <mohsen.babaei@podro.com>
2021-03-25 21:13:52 +0430

    menu-component WIP

---
+++ packages/core/src/components/menu/menu/menu.tunnel.tsx
@@ -0,0 +1,10 @@
+import {h} from "@stencil/core"
+import {createTunnel} from '@app/services/tunnel';
+export interface MenuTunnelState {
+  onClick?: (value: number) => number
+}
+
+export const MenuTunnel = createTunnel<MenuTunnelState>(
+  {},
+  (subscribe, child)=><plus-tunnel-consumer subscribe={subscribe} renderer={child} />
+)
---
+++ packages/core/src/components/menu/menu/menu.types.ts
@@ -0,0 +1,5 @@
+export type MenuAlignX = 'pointer' | 'start' | 'end' | 'left' | 'right' | 'center';
+export type MenuAlignY = 'pointer' | 'top' | 'center' | 'bottom';
+export type MenuGrowX = 'auto' | 'start' | 'end' | 'left' | 'right' | 'both';
+export type MenuGrowY = 'auto' | 'top' | 'both' | 'bottom';
+export type MenuTrigger = 'right-click' | 'left-click';
