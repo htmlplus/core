50b8b16d9e520ad0321d418422a1ca0350ff152f
mohsen babaei <mohsen.babaei@podro.com>
2021-04-04 12:36:26 +0430

    menu-component WIP

--- packages/core/src/components/menu/menu/menu.tsx
+++ packages/core/src/components/menu/menu/menu.tsx
@@ -1,9 +1,7 @@
-import {Component, Element, Event, EventEmitter, Host, Listen, Prop, State, h} from '@stencil/core';
-import {Bind, RectObserver} from '@app/services';
-import * as Utils from '@app/utils';
+import {Component, Element, Event, EventEmitter, Host, Prop, State, h} from '@stencil/core';
+import {Bind, RectObserver, ClickOutside} from '@app/services';
 import {MenuAlignX, MenuAlignY, MenuGrowX, MenuGrowY, MenuTrigger} from './menu.types';
-import {MenuTunnel, MenuTunnelState} from './menu.tunnel';
-// import {createPopper} from "@popperjs/core";
+import {createPopper} from '@popperjs/core';
 
 /**
  * TODO
@@ -91,8 +89,14 @@ export class Menu {
   @State()
   y?: string;
 
+  @State()
+  instance;
+  virtualElement = {getBoundingClientRect: this.generateGetBoundingClientRect()};
+
   @Element()
   $host!: HTMLElement;
+
+  $slot!: HTMLElement;
 
   $activator!: HTMLElement;
 
@@ -102,41 +106,8 @@ export class Menu {
 
   observer?: RectObserver;
 
-  get getGrowX() {
-
-    if (this.growX && this.growX !== 'auto') return this.growX;
-
-    const aligns = ['start', 'right', 'left', 'end'];
-
-    for (let i = 0; i < aligns.length; i++) {
-
-      const align = aligns[i];
-
-      if (this.alignX !== align) continue;
-
-      if (this.alignY === 'center') return align;
-
-      return aligns[aligns.length - 1 - i];
-    }
-
-    return 'both';
-  }
-
-  get getGrowY() {
-
-    if (this.growY && this.growY !== 'auto') return this.growY;
-
-    if (this.alignY !== 'center') return this.alignY;
-
-    return 'both';
-  }
-
   get reverse() {
     return getComputedStyle(this.$host).getPropertyValue('direction') === 'rtl';
-  }
-
-  get style() {
-    return this.s();
   }
 
   get eventTrigger() {
@@ -150,176 +121,69 @@ export class Menu {
     }
   }
 
-  // @Watch('fixed')
-  // @Watch('open')
   bind() {
-    // this.clean();
-    // if (!this.fixed || !this.open) return;
-    // this.observer = new RectObserver(this.update);
-    // requestAnimationFrame(() => this.observer.observe(this.$host));
-
-    this.$parent.addEventListener(this.eventTrigger, ()=>{})
+    ClickOutside.add(this.$host, this.hide)
+    document.addEventListener(this.eventTrigger, this.show)
   }
 
   unbind() {
-    this.$parent.removeEventListener(this.eventTrigger, ()=>{})
+    ClickOutside.remove(this.$host)
+    document.removeEventListener(this.eventTrigger, this.show)
   }
 
   startPopper() {
-
-  }
-
-  clean() {
-
-    if (!this.observer) return;
-
-    this.observer.disconnect();
-
-    this.observer = undefined;
-  }
-
-  click(event) {
-
-    const elements = Utils.eventPath(event);
-
-    const index = elements.findIndex((element) => element === this.$activator);
-
-    if (index < 2) return;
-
-    if (this.alignX === 'pointer') this.x = `${event.offsetX}px`;
-
-    if (this.alignY === 'pointer') this.y = `${event.offsetY}px`;
-
-    this.open = !this.open;
-
-    if (!this.open) this.plusClose.emit();
-  }
-
-  s(basex?: string, basey?: string) {
-
-    const constants = {
-      alignX: {
-        'start': !this.reverse ? '0%' : '100%',
-        'left': '0%',
-        'center': '50%',
-        'right': '100%',
-        'end': !this.reverse ? '100%' : '0%',
-      },
-      alignY: {
-        'top': '0%',
-        'center': '50%',
-        'bottom': '100%',
-      },
-      growX: {
-        'start': !this.reverse ? '-100%' : '0%',
-        'left': '-100%',
-        'both': '-50%',
-        'right': '0%',
-        'end': !this.reverse ? '0%' : '-100%',
-      },
-      growY: {
-        'top': '-100%',
-        'both': '-50%',
-        'bottom': '0%',
-      }
-    };
-
-    let x = constants.growX[this.getGrowX];
-
-    let y = constants.growY[this.getGrowY];
-
-    let top = basey || (this.alignY === 'pointer' ? this.y : constants.alignY[this.alignY]);
-
-    let left = basex || (this.alignX === 'pointer' ? this.x : constants.alignX[this.alignX]);
-
-    if (this.offsetX && !this.getGrowX.match(/both/)) {
-
-      const offset = Utils.toUnit(this.offsetX);
-
-      const operator = this.getGrowX.match(/left|start/) ? '-' : '+';
-
-      left = `calc(${left} ${operator} ${offset})`;
-    }
-
-    if (this.offsetY && !this.getGrowY.match(/both/)) {
-
-      const offset = Utils.toUnit(this.offsetY);
-
-      const operator = this.getGrowY.match(/top/) ? '-' : '+';
-
-      top = `calc(${top} ${operator} ${offset})`;
-    }
-
-    return {
-      top,
-      left,
-      transform: `translate(${x}, ${y})`,
-      position: this.fixed ? 'fixed' : 'absolute',
-      'z-index': '1',
-    };
+    this.instance = createPopper(this.$parent, this.$host);
   }
 
   @Bind
-  update(entries) {
+  show(ev) {
+    // let target = ev.target;
+    //
+    // console.log("target", target)
+    // do {
+    //   if (target == this.$parent) {
+    console.log("inside")
+    this.virtualElement.getBoundingClientRect= this.generateGetBoundingClientRect(ev.clientX, ev.clientY);
+    this.$host.setAttribute('data-show', '');
+    this.instance.setOptions({modifiers: [{name: 'eventListeners', enabled: true}]});
+    this.instance.update();
+    //     console.log("inside")
+    //     return;
+    //   }
+    //   target = target.preventNode;
+    // } while (target);
 
-    const content: any = this.$host.shadowRoot.querySelector('.content');
-
-    if (!content) return;
-
-    const rect = entries[0].boundingClientRect;
-
-    const style = this.s(rect.x.toString() + 'px', rect.y.toString() + 'px');
-
-    Object.keys(style).map((key) => this.$content.style[key] = style[key]);
+    // console.log("outside")
+    // this.hide();
   }
 
-  @Listen('click', {target: 'document', capture: true})
-  onOutsideClick(event) {
-
-    if (!this.open) return;
-
-    const path = Utils.eventPath(event);
-
-    const activator = this.$host.shadowRoot.querySelector('.activator');
-
-    if (path.find((element) => element === activator)) return;
-
-    if (this.persistent && path.find((element) => element === this.$host)) return;
-
-    const result = this.plusClose.emit();
-
-    if (!result.preventDefault) return;
-
-    this.open = false;
-
-    this.clean();
+  @Bind
+  hide() {
+    console.log("outside")
+    this.$host.removeAttribute('data-show');
+    this.instance.setOptions({modifiers: [{name: 'eventListeners', enabled: false}]});
+    this.instance.update();
   }
 
-  componentDidLoad() {
-    this.bind();
+  generateGetBoundingClientRect(x = 0, y = 0) {
+    return () => ({width: 0, height: 0, top: y, right: x, bottom: y, left: x,});
   }
 
   connectedCallback() {
-    this.bind();
+    console.log("parent", this.$slot)
+    console.log("$content", this.$content)
     this.startPopper();
+    this.bind();
   }
 
   disconnectedCallback() {
-    this.clean()
-  }
-
-  get state(): MenuTunnelState {
-    return {
-      onClick: (value: number) => value
-    }
   }
 
   render() {
     return (
       <Host>
-        <MenuTunnel.Provider state={this.state} scope={this}>
-          <slot/>
-        </MenuTunnel.Provider>
+        <slot name="content"/>
+        <slot/>
       </Host>
     );
   }
